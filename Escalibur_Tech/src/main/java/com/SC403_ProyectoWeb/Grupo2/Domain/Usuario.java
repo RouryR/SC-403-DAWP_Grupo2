c13bbf6b3685957f98f8222c97321c72e8723081
RouryR <Rouryffx@hotmail.com>
2023-10-29 19:07:34 -0600

    Update
    Folders and classes created:
    Dao, Controller, Domain, Service, ServiceImpl

---
+++ Escalibur_Tech/src/main/java/com/SC403_ProyectoWeb/Grupo2/Domain/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.SC403_ProyectoWeb.Grupo2.Domain;
+
+/**
+ *
+ * @author RouryR
+ */
+public class Usuario {
+    
+}
