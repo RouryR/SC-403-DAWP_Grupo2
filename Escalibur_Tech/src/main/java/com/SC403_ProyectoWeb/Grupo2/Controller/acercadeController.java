92439480b04dc30bf8f1cd0448c8ef72b5019e2d
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-11-05 01:03:07 -0600

    update 5/11/23
    hecha la página de acerca de nosotros

--- Escalibur_Tech/src/main/java/com/SC403_ProyectoWeb/Grupo2/Controller/acercadeController.java
+++ Escalibur_Tech/src/main/java/com/SC403_ProyectoWeb/Grupo2/Controller/acercadeController.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.SC403_ProyectoWeb.Grupo2.Controller;
 
-/**
- *
- * @author RouryR
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/acercade") //nombre de la carpeta
 public class acercadeController {
-    
+    @GetMapping("/nosotros") //nombre del html
+    public String acercade() {
+
+        return "/acercade/nosotros"; //Ruta completa
+    }
 }
