72013750e903169049aa2af92d690fc8bca9d566
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-11-26 18:16:48 -0600

    productos
    Se realizaron todos los html y controllers de los productos ofrecidos por la página. Cada controller esta ya terminado y hecho. Los HTML solo han sido creados, todavia no han sido modificados

---
+++ Escalibur_Tech/src/main/java/com/SC403_ProyectoWeb/Grupo2/Controller/FacturacionIntercompanyController.java
@@ -0,0 +1,15 @@
+package com.SC403_ProyectoWeb.Grupo2.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos") //nombre de la carpeta
+public class FacturacionIntercompanyController {
+    @GetMapping("/FacturacionIntercompany") //nombre del html
+    public String acercade() {
+
+        return "/productos/FacturacionIntercompany"; //Ruta completa
+    }
+}
---
+++ Escalibur_Tech/src/main/java/com/SC403_ProyectoWeb/Grupo2/Controller/InventarioController.java
@@ -0,0 +1,15 @@
+package com.SC403_ProyectoWeb.Grupo2.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos") //nombre de la carpeta
+public class InventarioController {
+    @GetMapping("/Inventario") //nombre del html
+    public String acercade() {
+
+        return "/productos/Inventario"; //Ruta completa
+    }
+}
